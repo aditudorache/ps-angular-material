e51bc680eee88774714758c74e463572832ad12d
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-12-31 10:55:04 +0100

    add notes

--- src/app/contactmanager/components/main-content/main-content.component.ts
+++ src/app/contactmanager/components/main-content/main-content.component.ts
@@ -6,29 +6,25 @@ import { UserService } from '../../services/user.service';
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
-  styleUrls: ['./main-content.component.scss']
+  styleUrls: ['./main-content.component.scss'],
 })
 export class MainContentComponent implements OnInit {
-
   user: User;
-  constructor(
-    private route: ActivatedRoute,
-    private service: UserService) { }
+  constructor(private route: ActivatedRoute, private service: UserService) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      const id = params['id'] | 1;
+    this.route.params.subscribe((params) => {
+      let id = params['id'];
+      if (!id) id = 1;
+      this.user = null;
 
-      this.service.users.subscribe(users => {
+      this.service.users.subscribe((users) => {
         if (users.length == 0) return;
 
         setTimeout(() => {
           this.user = this.service.userById(id);
-        }, 500)
-
+        }, 500);
       });
-
-    })
+    });
   }
-
 }
